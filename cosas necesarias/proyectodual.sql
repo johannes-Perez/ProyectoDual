27bc7fd063c29b07bc9bcc64cf1ca1e5b731f800
IsraelDAW <91871050+IsraelDAW@users.noreply.github.com>
2021-12-14 10:20:33 +0100

    crear base datos

--- cosas necesarias/proyectodual.sql
+++ cosas necesarias/proyectodual.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyectodual`
 --
-
+create database proyectodual;
+use proyectodual;
 -- --------------------------------------------------------
 
 --
