dd81964e544c294dad680b78ea0a1e2dc568cfe0
johannes-Perez <johannes.ma41082@cesurformacion.com>
2021-12-15 13:57:30 +0100

    separacion php y html

--- php/index.php
+++ php/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
 
     <!--Meta Tags Requeridos-->
@@ -11,28 +12,39 @@
     <link rel="stylesheet" href="../css/local.css">
 
 </head>
+
 <body>
 
-   
-<!--PNG logo Cesur-->
-<div> <img class="logocesur"  src="../img/CESUR-web.png" alt="Cesur Logo"></div>
 
-<div class="botonPadre">
+    <!--PNG logo Cesur-->
+    <div> <img class="logocesur" src="../img/CESUR-web.png" alt="Cesur Logo"></div>
 
-   <a href="añadir_alumno.php">      <input type="button"  class="btn btn-primary btn-lg padding" value="Añadir alumno"></a> <br>
+    <div class="botonPadre">
 
-   <a href="formularioProfesor.php"> <input type="button"  class="btn btn-primary btn-lg padding" value="Añadir profesor"></a> <br>
+        <a href="añadirAlumno.html"> <input type="button" class="btn btn-primary btn-lg padding"
+                value="Añadir alumno"></a> <br>
 
-    <a href="añadir_alumno.php">      <input type="button"  class="btn btn-primary btn-lg padding" value="Mostrar alumnos"></a> <br>
+        <a href="formularioProfesor.php"> <input type="button" class="btn btn-primary btn-lg padding"
+                value="Añadir profesor"></a> <br>
 
-    <a href="añadir_empresa.php">      <input type="button"  class="btn btn-primary btn-lg padding" value="añadir empresas"></a> <br>
 
-   <a href="añadir_alumno.php">       <input type="button"  class="btn btn-primary btn-lg padding" value="Mostrar empresas"></a> <br>
+        <a href="añadir_empresa.php"> <input type="button" class="btn btn-primary btn-lg padding"
+                value="añadir empresas"></a> <br>
 
-   <a href="añadir_alumno.php">      <input type="button"  class="btn btn-primary btn-lg padding" value="Mostrar actividad de alumnos"></a> <br>
+        <a href="añadir_alumno.php"> <input type="button" class="btn btn-primary btn-lg padding"
+                value="Mostrar empresas"></a> <br>
 
-   <a href="formularioActividad.php"><input type="button"  class="btn btn-primary btn-lg padding" value="Añadir nueva actividad"></a> 
 
-</div>
+        <a href="añadir_alumno.php"> <input type="button" class="btn btn-primary btn-lg padding"
+                value="Mostrar empresas"></a> <br>
+
+        <a href="añadir_alumno.php"> <input type="button" class="btn btn-primary btn-lg padding"
+                value="Mostrar actividad de alumnos"></a> <br>
+
+        <a href="formularioActividad.php"><input type="button" class="btn btn-primary btn-lg padding"
+                value="Añadir nueva actividad"></a>
+
+    </div>
 </body>
+
 </html>
