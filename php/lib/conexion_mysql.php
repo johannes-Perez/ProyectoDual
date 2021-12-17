6b0c9e97fb3a7d16d0e42001e023316333053d3c
Linosolis23 <lino.ma41393@cesurformacion.com>
2021-12-17 08:34:12 +0100

    Update conexion_mysql.php

--- php/lib/conexion_mysql.php
+++ php/lib/conexion_mysql.php
@@ -1,5 +1,7 @@
 <?php
     // Cambiad con vuestros datos de la base de datos
 
-    $conexion_mysql = new mysqli("localhost","elusuario","lacontra","proyectodual",3306);
+    //$conexion_mysql = new mysqli("localhost","elusuario","lacontra","proyectodual",3306);
+    //$conexion_mysql = new mysqli("localhost","root","","proyectodual",3307);
+
 ?>
