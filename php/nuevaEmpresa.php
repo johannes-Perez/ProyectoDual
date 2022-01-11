803bef312a8510b7ccd61d07982be5419020ad1d
IsraelDAW <israelantonio.ma44878@cesurformacion.com>
2022-01-11 10:31:23 +0100

    correción
    Son paginas php que redireccionan por lo que no necesitan html

--- php/nuevaEmpresa.php
+++ php/nuevaEmpresa.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-
-    <!--Meta Tags Requeridos-->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!--Link estilos boostrap-->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/local.css">
-    <body>
-           
-<div class="encabezado">
-       <img class="logo" alt="logo" src="../img/CESUR-web.png">
-    </div>
-
-    <div  class="menu">
-    <div id="contenido">
-                <a class="hoverAzul"href="../html/index.html">Home</a>
-                <a class="hoverAmarillo" href="mostrar_profesor.php">mostrar profesor</a>
-                <a class="hoverAzul"href="mostrar_alumno.php">mostrar alumno</a>
-                <a class="hoverAmarillo" href="mostrar_empresa.php">mostrar empresa</a>
-          </div>
-    </div>
-    </body>
-    <?php
+<?php
     // Falta Comprobar que sea un profesor el que accede a esta página (más tarde)
     
     require 'lib/conexion_mysql.php';
